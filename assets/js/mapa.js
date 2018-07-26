98ed150f73132d13650d2bcc38e0c9a482f3aaf9
Kaybel <kblmrtnz@gmail.com>
2018-07-26 11:14:53 -0400

    mapa sin errores, muestra marcadores de places.

--- assets/js/mapa.js
+++ assets/js/mapa.js
@@ -1,54 +1,59 @@
 // Instantiate the Platform class with authentication and
 // authorization credentials:
-let platform = new H.service.Platform({
+var platform = new H.service.Platform({
   useCIT: true,
-  app_id: '{pbx0QAZU3ePJRF70Wh4S} ',
-  app_code: '{iWBnPcO9Q3xiiNWlEK4Esg} '
+  app_id: 'EsgRmhCOsAbqTfPBeENf',
+  app_code: 'S7ODkhYBD-4VgAmQNseLxQ'
 });
 
 // Instantiate a map inside the DOM element with id map. The
-// map center is in Santiago, Chile, the zoom level is 10:
-let map = new H.Map(document.getElementById('map'),
-  platform.createDefaultLayers().normal.map, {
-    center: { lat: -33.447487, lng: -70.673676 },
-    zoom: 10
+// map center is in San Francisco, the zoom level is 10:
+// Obtain the default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
+var map = new H.Map(
+  document.getElementById('map'),
+  defaultLayers.normal.map,
+  {
+    zoom: 10,
+    center: { lat: -33.4569397, lng: -70.6482697 }
   });
 
 // Create a group object to hold map markers:
-let group = new H.map.Group();
+var group = new H.map.Group();
 
 // Create the default UI components:
-let ui = H.ui.UI.createDefault(map, platform.createDefaultLayers());
+var ui = H.ui.UI.createDefault(map, platform.createDefaultLayers());
 
 // Add the group object to the map:
 map.addObject(group);
 
 // Obtain a Search object through which to submit search
 // requests:
-let search = new H.places.Search(platform.getPlacesService()),
+var search = new H.places.Search(platform.getPlacesService()),
   searchResult, error;
 
 // Define search parameters:
-let params = {
+var params = {
   // Plain text search for places with the word "hotel"
   // associated with them:
-  'q': 'hotel',
-  'at': '-33.447487,-70.673676'
+  'q': 'eat',
+  //  Search in the Chinatown district in San Francisco:
+  'at': '-33.4569397,-70.6482697'
 };
 
 // Define a callback function to handle data on success:
-const onResult = ((data) => {
+function onResult(data) {
   addPlacesToMap(data.results);
-});
+}
 
 // Define a callback function to handle errors:
-const onError = ((data) => {
+function onError(data) {
   error = data;
-});
+}
 
 // This function adds markers to the map, indicating each of
 // the located places:
-const addPlacesToMap = ((result) => {
+function addPlacesToMap(result) {
   group.addObjects(result.items.map(function (place) {
     var marker = new H.map.Marker({
       lat: place.position[0],
@@ -56,7 +61,7 @@ const addPlacesToMap = ((result) => {
     })
     return marker;
   }));
-});
+}
 
 // Run a search request with parameters, headers (empty), and
 // callback functions:
